741a636ae884ccce1195523401a3cab2a7bbf75c
Matteo Crippa <matteocrippa@users.noreply.github.com>
2016-04-01 16:43:30 +0200

    changed name file

--- CacheManagerTests/CacheManagerTests.swift
+++ CacheManagerTests/CacheManagerTests.swift
@@ -1,36 +1,151 @@
 //
-//  CacheManagerTests.swift
-//  CacheManagerTests
+//  ManagerTests.swift
+//  AwesomeSwift
 //
-//  Created by Matteo Crippa on 01/04/2016.
+//  Created by Matteo Crippa on 31/03/2016.
 //  Copyright © 2016 boostco.de. All rights reserved.
 //
 
-import XCTest
+import Nimble
+import Quick
+import RealmSwift
+
 @testable import CacheManager
 
-class CacheManagerTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
+class ManagerTests: QuickSpec {
+    override func spec() {
+        
+        var sut: DummyManager!
+        var realm: Realm!
+        
+        var dummy: DummyObject!
+        var dummy2: DummyObject!
+        
+        beforeEach() {
+            sut = DummyManager()
+            
+            dummy = DummyObject()
+            dummy.name = "dummy"
+            dummy2 = DummyObject()
+            dummy2.name = "dummy2"
+            
+            realm = RealmProvider.realm()
+            // swiftlint:disable force_try
+            try! realm.write {
+                realm.deleteAll()
+            }
         }
+        
+        describe("manager") {
+            context("items counter") {
+                it("exists") {
+                    expect(sut.itemsCount).toNot(beNil())
+                }
+                it("is 0 at init") {
+                    expect(sut.itemsCount).to(equal(0))
+                }
+                it("is the same values of array items") {
+                    expect(sut.itemsCount).to(equal(sut.items.count))
+                }
+            }
+            context("items array") {
+                it("exists") {
+                    expect(sut.items).toNot(beNil())
+                }
+                it("can get item at index") {
+                    sut.itemAdd(dummy)
+                    let check = sut.itemAt(0)
+                    expect(sut.itemAt(0)).to(equal(check))
+                }
+                it("can't get item out of range") {
+                    sut.itemAdd(dummy)
+                    expect(sut.itemAt(7)).to(beNil())
+                }
+                it("can add item") {
+                    expect(sut.items.count).to(equal(0))
+                    sut.itemAdd(dummy)
+                    expect(sut.items.count).to(equal(1))
+                    expect(sut.itemAt(0)).to(equal(dummy))
+                }
+                it("can't add item more than once") {
+                    expect(sut.items.count).to(equal(0))
+                    sut.itemAdd(dummy)
+                    expect(sut.items.count).to(equal(1))
+                    sut.itemAdd(dummy)
+                    expect(sut.items.count).to(equal(1))
+                }
+                it("can remove item") {
+                    expect(sut.items.count).to(equal(0))
+                    sut.itemAdd(dummy)
+                    expect(sut.items.count).to(equal(1))
+                    sut.itemAdd(dummy2)
+                    expect(sut.items.count).to(equal(2))
+                    sut.itemRemoveAt(0)
+                    expect(sut.items.count).to(equal(1))
+                    sut.itemRemoveAt(0)
+                    expect(sut.items.count).to(equal(0))
+                }
+                it("can remove all items") {
+                    expect(sut.items.count).to(equal(0))
+                    sut.itemAdd(dummy)
+                    sut.itemAdd(dummy2)
+                    expect(sut.items.count).to(equal(2))
+                    sut.itemRemoveAll()
+                    expect(sut.items.count).to(equal(0))
+                }
+                it("can't remove out of range") {
+                    expect(sut.items.count).to(equal(0))
+                    sut.itemAdd(dummy)
+                }
+            }
+            context("realm cache") {
+                it("exists") {
+                    expect(sut.realm).toNot(beNil())
+                }
+                it("add added") {
+                    expect(sut.realm.objects(DummyObject).count).to(equal(0))
+                    sut.itemAdd(dummy)
+                    expect(sut.realm.objects(DummyObject).count).to(equal(1))
+                }
+                it("removes delete") {
+                    expect(sut.realm.objects(DummyObject).count).to(equal(0))
+                    sut.itemAdd(dummy)
+                    expect(sut.realm.objects(DummyObject).count).to(equal(1))
+                    sut.itemRemoveAll()
+                    expect(sut.realm.objects(DummyObject).count).to(equal(0))
+                }
+                it("handle cache") {
+                    expect(sut.realm.objects(DummyObject).count).to(equal(0))
+                    sut.itemAdd(dummy)
+                    expect(sut.realm.objects(DummyObject).count).to(equal(1))
+                    expect(sut.itemsCount).to(equal(1))
+                    // test removing only item locally and not in cache
+                    sut.items.removeAll()
+                    expect(sut.realm.objects(DummyObject).count).to(equal(1))
+                    expect(sut.itemsCount).to(equal(0))
+                    // try to restore locally the realm cache
+                    sut.itemsFromCache()
+                    expect(sut.realm.objects(DummyObject).count).to(equal(1))
+                    expect(sut.itemsCount).to(equal(1))
+                }
+            }
+        }
+    }
+}
+
+class DummyObject: Object {
+    dynamic var name: String = ""
+}
+
+class DummyManager: CacheManager {
+    override init() {
+        super.init()
+        items = [DummyObject]()
+    }
+    
+    override func itemsFromCache() {
+        // swiftlint:disable force_try
+        items = Array(try! realm.objects(DummyObject))
     }
     
 }
