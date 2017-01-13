792af859bad49c1820f9626b8574bcc15c34006b
Matteo Crippa <matteo@boostco.de>
2017-01-13 23:35:44 +0100

    nserror to string replace

--- CacheManager/CacheManager.swift
+++ CacheManager/CacheManager.swift
@@ -53,7 +53,7 @@ open class CacheManager<T> where T: Object {
         syncCacheItems()
     }
 
-    open func getRemoteItems(_ completion: (_ error: NSError?)->Void) {}
+    open func getRemoteItems(_ completion: (_ error: String?)->Void) {}
 }
 
 extension CacheManager {
