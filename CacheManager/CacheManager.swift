50cdc2cd8c2476e00943a8dc6b77481e1a7f8ba4
Matteo Crippa <matteocrippa@users.noreply.github.com>
2019-11-11 20:04:17 +0100

    updated for swift 5.1

--- CacheManager/CacheManager.swift
+++ CacheManager/CacheManager.swift
@@ -10,177 +10,177 @@ import Foundation
 import RealmSwift
 
 public protocol CacheManagerDelegate {
-  func cacheHasUpdate()
+    func cacheHasUpdate()
 }
 
 open class CacheManager<T> where T: Object {
-  
-  fileprivate var realm = RealmProvider.realm()
-  
-  var items: Results<T>! {
-    didSet {
-      delegate?.cacheHasUpdate()
+    
+    fileprivate var realm = RealmProvider.realm()
+    
+    var items: Results<T>! {
+        didSet {
+            delegate?.cacheHasUpdate()
+        }
     }
-  }
-  
-  open var count: Int {
-    return items.count
-  }
-  
-  open var delegate: CacheManagerDelegate?
-  
-  open var ignoreOnUpdate: [String] = []
-  
-  open var sort: String? {
-    didSet {
-      syncCacheItems()
+    
+    open var count: Int {
+        return items.count
     }
-  }
-  
-  open var sortAscending: Bool? {
-    didSet {
-      syncCacheItems()
+    
+    open var delegate: CacheManagerDelegate?
+    
+    open var ignoreOnUpdate: [String] = []
+    
+    open var sort: String? {
+        didSet {
+            syncCacheItems()
+        }
     }
-  }
-  
-  open var filter: NSPredicate? {
-    didSet {
-      syncCacheItems()
+    
+    open var sortAscending: Bool? {
+        didSet {
+            syncCacheItems()
+        }
     }
-  }
-  
-  public init() {
-    syncCacheItems()
-  }
-  
-  open func getRemoteItems(_ completion: @escaping (_ error: String?) -> ()) {}
+    
+    open var filter: NSPredicate? {
+        didSet {
+            syncCacheItems()
+        }
+    }
+    
+    public init() {
+        syncCacheItems()
+    }
+    
+    open func getRemoteItems(_ completion: @escaping (_ error: String?) -> ()) {}
 }
 
 extension CacheManager {
-  fileprivate func getCacheItems<T: Object>(_ type: T.Type) -> Results<T> {
-    return realm.objects(T.self)
-  }
-  
-  fileprivate func syncCacheItems() {
-    var tmp = getCacheItems(T.self)
-    
-    if let filter = self.filter {
-      tmp = tmp.filter(filter)
+    fileprivate func getCacheItems<T: Object>(_ type: T.Type) -> Results<T> {
+        return realm.objects(T.self)
     }
     
-    if let order = self.sort {
-      tmp = tmp.sorted(byKeyPath: order, ascending: sortAscending!)
+    fileprivate func syncCacheItems() {
+        var tmp = getCacheItems(T.self)
+        
+        if let filter = self.filter {
+            tmp = tmp.filter(filter)
+        }
+        
+        if let order = self.sort {
+            tmp = tmp.sorted(byKeyPath: order, ascending: sortAscending!)
+        }
+        
+        items = tmp
+        
     }
-    
-    items = tmp
-    
-  }
 }
 
 // MARK: - Getter
 extension CacheManager {
-  public func itemAll() -> [T?] {
-    return items.map({ $0 })
-  }
-  public func itemAt(_ index: Int) -> T? {
-    if 0..<count ~= index {
-      return items[index]
-    } else {
-      return nil
+    public func itemAll() -> [T?] {
+        return items.map({ $0 })
     }
-  }
-  public func itemFirst() -> T? {
-    if count > 0 {
-      return items[0]
-    } else {
-      return nil
+    public func itemAt(_ index: Int) -> T? {
+        if 0..<count ~= index {
+            return items[index]
+        } else {
+            return nil
+        }
     }
-  }
-  public func itemLast() -> T? {
-    if count > 0 {
-      return items[count-1]
-    } else {
-      return nil
+    public func itemFirst() -> T? {
+        if count > 0 {
+            return items[0]
+        } else {
+            return nil
+        }
     }
-  }
+    public func itemLast() -> T? {
+        if count > 0 {
+            return items[count-1]
+        } else {
+            return nil
+        }
+    }
 }
 
 // MARK: - Setters
 extension CacheManager {
-  public func itemAdd(_ item: T, forceSync: Bool = true) {
-    // swiftlint:disable force_try
-    try! realm.write {
-      
-      if let currentObject = realm.object(ofType: T.self, forPrimaryKey: item[T.primaryKey()!]! as AnyObject) {
-        
-        for ignore in ignoreOnUpdate {
-          if let current = currentObject[ignore] {
-            item[ignore] = current
-          }
+    public func itemAdd(_ item: T, forceSync: Bool = true) {
+        // swiftlint:disable force_try
+        try! realm.write {
+            
+            if let currentObject = realm.object(ofType: T.self, forPrimaryKey: item[T.primaryKey()!]! as AnyObject) {
+                
+                for ignore in ignoreOnUpdate {
+                    if let current = currentObject[ignore] {
+                        item[ignore] = current
+                    }
+                }
+            }
+            
+            realm.add(item, update: .all)
+            if forceSync == true {
+                syncCacheItems()
+            }
         }
-      }
-      
-      realm.add(item, update: true)
-      if forceSync == true {
+    }
+    public func itemAddFromArray(_ items: [T]) {
+        for item in items {
+            itemAdd(item, forceSync: false)
+        }
         syncCacheItems()
-      }
     }
-  }
-  public func itemAddFromArray(_ items: [T]) {
-    for item in items {
-      itemAdd(item, forceSync: false)
+    public func itemUpdate(_ item: T) {
+        // swiftlint:disable force_try
+        try! realm.write {
+            realm.add(item, update: .all)
+            syncCacheItems()
+        }
     }
-    syncCacheItems()
-  }
-  public func itemUpdate(_ item: T) {
-    // swiftlint:disable force_try
-    try! realm.write {
-      realm.add(item, update: true)
-      syncCacheItems()
+    public func itemUpdate(_ item: T, key: String, value: Any) {
+        try! realm.write {
+            item[key] = value
+            syncCacheItems()
+            delegate?.cacheHasUpdate()
+        }
     }
-  }
-  public func itemUpdate(_ item: T, key: String, value: Any) {
-    try! realm.write {
-      item[key] = value
-      syncCacheItems()
-      delegate?.cacheHasUpdate()
+    public func itemRemove(_ item: T) {
+        // swiftlint:disable force_try
+        try! realm.write {
+            realm.delete(item)
+            syncCacheItems()
+        }
     }
-  }
-  public func itemRemove(_ item: T) {
-    // swiftlint:disable force_try
-    try! realm.write {
-      realm.delete(item)
-      syncCacheItems()
+    public func itemRemoveAll() {
+        // swiftlint:disable force_try
+        try! realm.write {
+            realm.delete(realm.objects(T.self))
+            syncCacheItems()
+        }
     }
-  }
-  public func itemRemoveAll() {
-    // swiftlint:disable force_try
-    try! realm.write {
-      realm.delete(realm.objects(T.self))
-      syncCacheItems()
-    }
-  }
-  
+    
 }
 
 // MARK: - Realm handler
 class RealmProvider {
-  class func realm() -> Realm {
-    if let _ = NSClassFromString("QuickSpec") {
-      // swiftlint:disable force_try
-      return try! Realm(
-        configuration: Realm.Configuration(
-          fileURL: nil,
-          inMemoryIdentifier: "test",
-          encryptionKey: nil,
-          readOnly: false,
-          schemaVersion: 0,
-          migrationBlock: nil,
-          objectTypes: nil
-      ))
-    } else {
-      // swiftlint:disable force_try
-      return try! Realm()
+    class func realm() -> Realm {
+        if let _ = NSClassFromString("QuickSpec") {
+            // swiftlint:disable force_try
+            return try! Realm(
+                configuration: Realm.Configuration(
+                    fileURL: nil,
+                    inMemoryIdentifier: "test",
+                    encryptionKey: nil,
+                    readOnly: false,
+                    schemaVersion: 0,
+                    migrationBlock: nil,
+                    objectTypes: nil
+            ))
+        } else {
+            // swiftlint:disable force_try
+            return try! Realm()
+        }
     }
-  }
 }
